b95f8b6e12226e11e63b2d91014faa7f7d1ea758
Vova <vladimirgolicenko154@gmail.com>
2020-09-08 21:58:51 +0300

    homework17 - json <-> yaml parser, UnitTests for parser.

--- src/main/java/parser/ReadFromFile.java
+++ src/main/java/parser/ReadFromFile.java
@@ -7,16 +7,12 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class ReadFromFile {
-    public static String readToString(String filePath)
-    {
+    public static String readToString(String filePath) {
         StringBuilder contentBuilder = new StringBuilder();
 
-        try (Stream<String> stream = Files.lines( Paths.get(filePath), StandardCharsets.UTF_8))
-        {
+        try (Stream<String> stream = Files.lines(Paths.get(filePath), StandardCharsets.UTF_8)) {
             stream.forEach(s -> contentBuilder.append(s).append("\n"));
-        }
-        catch (IOException e)
-        {
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
