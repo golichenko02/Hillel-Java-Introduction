c79e2485020a3529981ee4a2e96437566400a9a0
Vova <vladimirgolicenko154@gmail.com>
2020-09-17 15:24:51 +0300

    homework19 - producer <-> consumer

--- multithreading/src/main/java/hw19/Main.java
+++ multithreading/src/main/java/hw19/Main.java
@@ -5,7 +5,7 @@ import java.util.LinkedList;
 public class Main {
     public static void main(String[] args) {
         LinkedList<Product> sharedQueue = new LinkedList<>();
-        int size = 10;
+        int size = 3;
         Thread prodThread = new Thread(new Producer(sharedQueue, size), "Producer");
         Thread consThread = new Thread(new Consumer(sharedQueue), "Consumer");
         prodThread.start();
